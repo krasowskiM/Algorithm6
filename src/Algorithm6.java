c7aff1376ea19d2a7f699337d93b40c4c78fdece
krasowskiM <krasowski.m87@gmail.com>
2015-11-23 17:33:52 +0100

    Added "do while" loop in main method and some other fixes

--- src/Algorithm6.java
+++ src/Algorithm6.java
@@ -4,14 +4,13 @@ import java.util.*;
 
 public class Algorithm6 {
     
-       private static void switcher(int input){ 
+       private static void switcher(List list, int input){ 
         
-        List list = new List();
         
-        while (input != 0) {
+              
         switch (input) {
             
-            case 0:
+            case 1:
                 String name;
                 int quantity;
                 System.out.print("Please put in the name of the item: \n");
@@ -23,31 +22,31 @@ public class Algorithm6 {
                 list.insertFirstLink(name, quantity);
                 break;
                 
-            case 1:
+            case 2:
                 System.out.print("Here's your list: \n");
                 list.display();
                 break;
             
-            case 2:
+            case 3:
                 System.out.print("Which item you would like to remove?\n");
                 Scanner input3 = new Scanner(System.in);
                 name = input3.nextLine();
                 list.removeLink(name);
                 break;
             
-            case 3:
+            case 4:
                 System.out.print("Which item are you searching for?");
                 Scanner input4 = new Scanner(System.in);
                 name = input4.nextLine();
                 list.find(name);
                 break;
             
-            case 4:
+            case 5:
                 list.removeFirst();
                 System.out.print("First item from the list removed.");
                 break;
             }
-        }
+        
     }        
        
 
@@ -56,15 +55,23 @@ public class Algorithm6 {
        
         System.out.print("Welcome to your virtual storage! Please choose the operation you would like to perform.\n"
                 + "List of operations:\n"
-                + "-create next item(creates new items in the list)\n"
-                + "-display(displays the full content of the list)\n"
-                + "-remove item(removes item from the list)\n"
-                + "-remove first item from the list\n"
-                + "-find item(finds item on the list)\n");
+                + "1-create next item(creates new items in the list)\n"
+                + "2-display(displays the full content of the list)\n"
+                + "3-remove item(removes item from the list)\n"
+                + "4-remove first item from the list\n"
+                + "5-find item(finds item on the list)\n"
+                + "0-exit program\n");
+        
+        List list = new List();
         int input;
+        do {
+        System.out.print("Put in the number of the operation: \n");
         Scanner inNew = new Scanner(System.in);
         input = inNew.nextInt();
-        switcher(input);
+        switcher(list, input);
+        System.out.println();
+        } while (input != 0);
+        System.out.print("Program terminated by user.");
     }
     
 }
